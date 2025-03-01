4952f619369d137bb1bbb1801a1680c7464c2f35
magnus <magnus@udel.edu>
2025-03-01 15:22:13 -0500

    Orwell, now responds to Orwell,

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+from google import genai
 
+client = genai.Client(api_key="AIzaSyCcRitudApXCvU63ocM5aV-iViBuxmigik")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="-", intents=intents)
 
@@ -8,8 +10,17 @@ bot = commands.Bot(command_prefix="-", intents=intents)
 async def on_ready():
     print("online")
 
-@bot.command(name="ping")
-async def ping(ctx):
-    await ctx.send("pong")
+
+@bot.event
+async def on_message(message):
+    user_message = message.content
+    print(f"User said: {user_message}")
+    if "orwell, " in user_message.lower():
+        print("1")
+        response=client.models.generate_content(model="gemini-2.0-flash", contents= "be very brief:"+message.content)
+        print("2")
+        await message.channel.send(response.text[:2000])
+    print("3")
+    await bot.process_commands(message)
 
 bot.run("X")
