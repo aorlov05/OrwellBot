c98f678e46e709c2fa5f9717ba346d24b890092d
Limenade360 <limenade360@gmail.com>
2025-03-01 17:12:12 -0500

    timeout function implemented

--- moderation.py
+++ moderation.py
@@ -103,17 +103,17 @@ class Moderation(commands.Cog):
                 await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.has_permissions(kick_members=True)
-    @commands.bot_has_permissions(kick_members=True)
-    @app_commands.describe(user="The user to kick.", reason="The reason for kicking.")
-    async def mute(self, ctx, user: discord.User, reason: str = "No Reason Given") -> None:
+    @commands.has_permissions(timeout_members=True)
+    @commands.bot_has_permissions(timeout_members=True)
+    @app_commands.describe(user="The user to timeout.", reason="The reason for timeout.")
+    async def timeout(self, ctx, user: discord.User, reason: str = "No Reason Given") -> None:
         """
-        Kick a user from the server
+        timeout a user from the server
 
         Parameters:
         ctx: the command context
-        user: the user to kick from the server
-        reason: the reason to kick the user, defaults to "No Reason Given"
+        user: the user to timeout from the server
+        reason: the reason to timeout the user, defaults to "No Reason Given"
 
         Returns:
         None
@@ -122,30 +122,30 @@ class Moderation(commands.Cog):
         # Check if the user is an admin
         if member.guild_permissions.administrator:
             embed = discord.Embed(
-                description="Cannot kick, User is an administrator.", color=0xFF0000
+                description="Cannot timeout, User is an administrator.", color=0xFF0000
             )
             await ctx.send(embed=embed)
         else:
-            # Try kicking user
+            # Try timing out user
             try:
-                await ctx.guild.kick(user, reason=reason)
+                await ctx.guild.timeout(user, reason=reason)
                 embed = discord.Embed(
-                    description=f"**{member}** was kicked by **{ctx.author}**!",
+                    description=f"**{member}** was timed out by **{ctx.author}**!",
                     color=0x00FF00
                 )
                 embed.add_field(name="Reason:", value=reason)
                 await ctx.send(embed=embed)
-                # Try dming user with kick message
+                # Try dming user with timeout message
                 try:
                     await member.send(
-                        f"You were kicked by **{ctx.author}** from **{ctx.guild.name}**!\nReason: {reason}"
+                        f"You were timed out by **{ctx.author}** from **{ctx.guild.name}**!\nReason: {reason}"
                     )
                 except:
                     # Couldn't DM user
                     pass
             except:
                 embed = discord.Embed(
-                    description="An error occured trying to kick the user, make sure Orwell has permission to kick this user.",
+                    description="An error occured trying to timeout the user, make sure Orwell has permission to timeout this user.",
                     color=0xFF0000,
                 )
                 await ctx.send(embed=embed)
