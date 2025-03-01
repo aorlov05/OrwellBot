37774e7ee00327bbcb7d6a0fe72cdeec1ed3f439
Limenade360 <limenade360@gmail.com>
2025-03-01 17:05:39 -0500

    implemented ban function

--- moderation.py
+++ moderation.py
@@ -55,17 +55,17 @@ class Moderation(commands.Cog):
                 await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.has_permissions(kick_members=True)
-    @commands.bot_has_permissions(kick_members=True)
-    @app_commands.describe(user="The user to kick.", reason="The reason for kicking.")
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    @app_commands.describe(user="The user to ban.", reason="The reason for banning.")
     async def ban(self, ctx, user: discord.User, reason: str = "No Reason Given") -> None:
         """
-        Kick a user from the server
+        Ban a user from the server
 
         Parameters:
         ctx: the command context
-        user: the user to kick from the server
-        reason: the reason to kick the user, defaults to "No Reason Given"
+        user: the user to ban from the server
+        reason: the reason to ban the user, defaults to "No Reason Given"
 
         Returns:
         None
@@ -74,30 +74,30 @@ class Moderation(commands.Cog):
         # Check if the user is an admin
         if member.guild_permissions.administrator:
             embed = discord.Embed(
-                description="Cannot kick, User is an administrator.", color=0xFF0000
+                description="Cannot ban, User is an administrator.", color=0xFF0000
             )
             await ctx.send(embed=embed)
         else:
-            # Try kicking user
+            # Try banning user
             try:
-                await ctx.guild.kick(user, reason=reason)
+                await ctx.guild.ban(user, reason=reason)
                 embed = discord.Embed(
-                    description=f"**{member}** was kicked by **{ctx.author}**!",
+                    description=f"**{member}** was banned by **{ctx.author}**!",
                     color=0x00FF00
                 )
                 embed.add_field(name="Reason:", value=reason)
                 await ctx.send(embed=embed)
-                # Try dming user with kick message
+                # Try dming user with ban message
                 try:
                     await member.send(
-                        f"You were kicked by **{ctx.author}** from **{ctx.guild.name}**!\nReason: {reason}"
+                        f"You were banned by **{ctx.author}** from **{ctx.guild.name}**!\nReason: {reason}"
                     )
                 except:
                     # Couldn't DM user
                     pass
             except:
                 embed = discord.Embed(
-                    description="An error occured trying to kick the user, make sure Orwell has permission to kick this user.",
+                    description="An error occured trying to ban the user, make sure Orwell has permission to ban this user.",
                     color=0xFF0000,
                 )
                 await ctx.send(embed=embed)
