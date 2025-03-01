da8f116f10edc96e87c3912c95490c37f02fd210
magnus <magnus@udel.edu>
2025-03-01 13:47:39 -0500

    Orwell can go online and read messages

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hi!")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="-", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("online")
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run("X")
