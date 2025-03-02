10c782d7eba38ad29808c1bd2238856c691cac04
aorlov05 <aorlov@udel.edu>
2025-03-01 21:42:12 -0500

    Add function to detect profanity in message

--- filter.py
+++ filter.py
@@ -11,7 +11,7 @@ def call_smalltalk_is_http(message):
     message: The content of a user message
 
     Returns:
-    If the message is a link starting with http, https, or www.
+    If the message is a link starting with http, https, or www, returns True.
     This also returns silently returns False if GNU Smalltalk is not installed on the OS.
     """
     # Open the Smalltalk file to read the existing content
@@ -36,3 +36,28 @@ def call_smalltalk_is_http(message):
             os.remove(random_file_name)
     # If stdout=False, url was not found, therwise url pattern matched.
     return "False" not in str(result)
+
+
+def check_profanity(mongo_client, message):
+    """
+    Returns a list of all profane words in a message with their corresponding severities
+    :param mongo_client: A PyMongo MongoClient object to access the database
+    :param message: The message to check
+    :return: A list containing all profane words and their severities in a list
+    """
+    db = mongo_client["filter"]
+    profanity = db["profanity"]
+
+    detected_profanity = []
+    # Fetch all profane words with rating and description
+    for doc in profanity.find({}, {"text": 1, "severity_rating": 1, "severity_description": 1}):
+        profanity_word = doc.get("text", "")
+        # Check for profanity in the message
+        if profanity_word.lower() in message.lower():
+            # Add to list which contains the word and severity
+            detected_profanity.append({
+                "word": profanity_word,
+                "severity_rating": doc["severity_rating"],
+                "severity_description": doc["severity_description"]
+            })
+    return detected_profanity
--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import os
 import discord
 from discord.ext import commands
 from google import genai
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 from dotenv import load_dotenv
 load_dotenv()
 DISCORD_API_KEY = os.environ.get('DISCORD_API_KEY')
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
+MONGODB_URI = os.environ.get('MONGODB_URI')
 
 from moderation import Moderation
 
-client = genai.Client(api_key="")
+client = genai.Client(api_key=GEMINI_API_KEY)
+mongo_client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="-", intents=intents)
 
 @bot.event
 async def on_ready():
+    try:
+        mongo_client.admin.command('ping')
+        print("Successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
     await bot.add_cog(Moderation(bot))
     print("online")
 
